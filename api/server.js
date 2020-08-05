7d12b5ce2660de2bab4defd4a378a52ad11aad3c
Chase <ccviol01@gmail.com>
2020-08-04 23:59:34 -0700

    removing unnecessary server info

--- api/server.js
+++ api/server.js
@@ -25,56 +25,6 @@ app.get('/electronics', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/electronics.html'));
 });
 
-// delete this later
-app.get('/submit', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/form.html'));
-});
-
-// delete this later
-app.post('/submit', (req, res) => {
-  console.log({
-    name: req.body.name,
-    message: req.body.message
-  });
-  res.send('Thanks for your message!... ' + req.body.name + " : " + req.body.message);
-});
-
-async function audience() {
-  if (!aud && (await metadata.isAvailable())) {
-    let project_number = await metadata.project('numeric-project-id');
-    let project_id = await metadata.project('project-id');
-
-    aud = '/projects/' + project_number + '/apps/' + project_id;
-  }
-
-  return aud;
-}
-
-async function validateAssertion(assertion) {
-  if (!assertion) {
-    return {};
-  }
-
-  // Check that the assertion's audience matches ours
-  const aud = await audience();
-
-  // Fetch the current certificates and verify the signature on the assertion
-  const response = await oAuth2Client.getIapPublicKeys();
-  const ticket = await oAuth2Client.verifySignedJwtWithCertsAsync(
-    assertion,
-    response.pubkeys,
-    aud,
-    ['https://cloud.google.com/iap']
-  );
-  const payload = ticket.getPayload();
-
-  // Return the two relevant pieces of information
-  return {
-    email: payload.email,
-    sub: payload.sub,
-  };
-}
-
 app.get('/login', async (req, res) => {
   res.sendFile(path.join(__dirname, '/views/login.html'));
 });
